e19893aab7fc611cd5615464b6d7a69beb160f6c
Peters <gp723@ic.ac.uk>
2026-03-03 11:57:50 +0000

    Added integral and derivative gains

---
+++ MOTOR.s
@@ -0,0 +1,37 @@
+#include <xc.inc>
+
+global move_motor_X, move_motor_Y
+    
+extrn  TARGET_X_L, TARGET_X_H, TARGET_Y_L, TARGET_Y_H
+
+ 
+psect motor_code, class=CODE
+    
+move_motor_X:
+    
+    movf TARGET_X_L, W, A
+    cpfsgt TMR0L
+    return
+    
+    movf TARGET_X_H, W
+    cpfsgt  TMR0H ; compare w and f, skip if greater than 
+    return
+    
+    bcf PORTD, 2, A
+    return
+    
+
+move_motor_Y:
+    movf TARGET_Y_L, W, A
+    cpfsgt TMR0L
+    return
+    
+    movf TARGET_Y_H, W
+    cpfsgt  TMR0H ; compare w and f, skip if greater than 
+    return
+    
+    bcf PORTD, 3, A
+    return
+
+
+    end
--- main.s
+++ main.s
@@ -1,27 +1,57 @@
 #include <xc.inc>
-
-global ACCEL_X_H, ACCEL_X_L, ACCEL_Y_H, ACCEL_Y_L    
+    
+global ACCEL_X_H, ACCEL_X_L, ACCEL_Y_H, ACCEL_Y_L
+global TARGET_X_H, TARGET_X_L, TARGET_Y_H, TARGET_Y_L
+global TOTAL_X_L, TOTAL_X_H, TOTAL_Y_L, TOTAL_Y_H
     
 extrn	DAC_Setup, DAC_Int_Hi
 extrn	Setup_Accel, Read_Accel
 extrn	UART_Setup, UART_Transmit_Message
+extrn   move_motor_X, move_motor_Y
     
 psect udata_acs
+SHIFT_H: ds 1
+SHIFT_L: ds 1
+    
 delay_count: ds 1
 delay_count_2: ds 1
 delay_count_3: ds 1
-ACCEL_X_L: ds 1
-ACCEL_X_H: ds 1
-ACCEL_Y_L: ds 1
-ACCEL_Y_H: ds 1
-AVERAGE_L: ds 1
-AVERAGE_H: ds 1
-TEMP_H: ds 1
-TEMP_L: ds 1
+
 DEFAULT_H: ds 1
 DEFAULT_L: ds 1
 counter:    ds 1
 UART_counter: ds 1
+    
+; Variables for PWM signal
+TARGET_X_L: ds 1
+TARGET_X_H: ds 1
+TARGET_Y_L: ds 1
+TARGET_Y_H: ds 1
+
+; Tilt values (distance from 0) for potential gain
+ACCEL_X_L: ds 1
+ACCEL_X_H: ds 1
+ACCEL_Y_L: ds 1
+ACCEL_Y_H: ds 1
+TEMP_H: ds 1
+TEMP_L: ds 1
+    
+; Change in accelerometer reading for differential gain
+DELTA_X_L: ds 1
+DELTA_X_H: ds 1
+DELTA_Y_H: ds 1
+DELTA_Y_L: ds 1
+TEMP_DELTA_L: ds 1
+TEMP_DELTA_H: ds 1
+    
+; Total error for integration gain
+TOTAL_X_H: ds 1
+TOTAL_X_L: ds 1
+TOTAL_Y_H: ds 1
+TOTAL_Y_L: ds 1
+TEMP_TOTAL_L: ds 1
+TEMP_TOTAL_H: ds 1
+    
     
 psect	code, abs
 rst:	org	0x0000	; reset vector
@@ -33,8 +63,14 @@ int_hi:	org	0x0008	; high vector, no low vector
 start:
     
     movlw 0x00
-    movwf AVERAGE_L, A
-    movwf AVERAGE_H, A
+    movwf DELTA_X_L, A
+    movwf DELTA_X_H, A
+    movwf DELTA_Y_L, A
+    movwf DELTA_Y_H, A
+    movwf TOTAL_X_L, A
+    movwf TOTAL_X_H, A
+    movwf TOTAL_Y_L, A
+    movwf TOTAL_Y_H, A
     
     movlw 0x6F
     movwf DEFAULT_H, A
@@ -57,13 +93,22 @@ start:
     call	UART_Setup
     call	Setup_Accel
     call	DAC_Setup
+    
+    
    
     
 loop:
-    call Read_Accel
 
+    ; Writes to UART every 256 cycles
     decfsz UART_counter, A
     bra skip_UART
+    
+    ; For the same 256 cycles, set integration back to 0???
+    ;movlw 0x00
+    ;movwf TOTAL_X_H, A
+    ;movwf TOTAL_X_L, A
+    ;movwf TOTAL_Y_H, A
+   ;movwf TOTAL_Y_L, A
     
     movlw 0xFF
     movwf UART_counter
@@ -73,83 +118,131 @@ loop:
     call UART_Transmit_Message
     
 skip_UART:
-    ; Load new X sample into TEMP
+    
+    ; Set the current tilt values to the delta values
+    movff ACCEL_X_H, DELTA_X_H
+    movff ACCEL_X_L, DELTA_X_L
+    movff ACCEL_Y_H, DELTA_Y_H
+    movff ACCEL_Y_L, DELTA_Y_L
+    
+    call Read_Accel
+    
+    
+    ; Now subtract the NEW tilt values from delta values
+    movf ACCEL_X_L, W, A
+    subwf DELTA_X_L, A
+    
+    movf ACCEL_X_H, W, A
+    subwfb DELTA_X_H, A
+    
+    movf ACCEL_Y_L, W, A
+    subwf DELTA_Y_L, A
+    
+    movf ACCEL_Y_H, W, A
+    subwfb DELTA_Y_H, A
+    
+    
+    ; Add to total tilt values for integration gain
+    ; ONLY IF DELTA A IS SMALL - near equilibrium!
+    
+    tstfsz DELTA_X_H, A
+    bra skip_integration_X
+    
+    movlw 0x70
+    cpfslt TOTAL_X_H, A
+    bra skip_integration_X
+    
+    ; Add small portion of the error to the total
+    ;rrcf ACCEL_X_H
+    
+    ;movf ACCEL_X_L, W, A
+    ;addwf TOTAL_X_L, F
+    
+    ;movf ACCEL_X_H, W, A
+    ;addwfc TOTAL_X_H, F
+    
+skip_integration_X:    
+    
+    tstfsz DELTA_Y_H, A
+    bra skip_integration_Y
+    
+    movlw 0x70
+    cpfslt TOTAL_Y_H, A
+    bra skip_integration_Y
+    
+    movf ACCEL_Y_L, W, A
+    addwf TOTAL_Y_L, F
+    
+    movf ACCEL_Y_H, W, A
+    addwfc TOTAL_Y_H, F
+    
+ skip_integration_Y:
+
+    
+    ; Convert X acceleration into motor servo PWM
     movff ACCEL_X_H, TEMP_H
     movff ACCEL_X_L, TEMP_L
-
-    ; EMA: TEMP = TEMP - AVERAGE
-    movf AVERAGE_L, W, A
-    subwf TEMP_L, F
-    movf AVERAGE_H, W, A
-    subwfb TEMP_H, F
-
-    ; Divide by 16
-    movlw 0x09
-    call right_shift_W
-
-    ; AVERAGE = AVERAGE + TEMP
-    movf TEMP_L, W, A
-    addwf AVERAGE_L, F, A
-    movf TEMP_H, W, A
-    addwfc AVERAGE_H, F, A
-
-    ; Use filtered average as input to convert_tilt
-    movff AVERAGE_H, TEMP_H
-    movff AVERAGE_L, TEMP_L
-
+    movff DELTA_X_H, TEMP_DELTA_H
+    movff DELTA_X_L, TEMP_DELTA_L
+    movff TOTAL_X_H, TEMP_TOTAL_H
+    movff TOTAL_X_L, TEMP_TOTAL_L
     call convert_tilt
-
-    movff TEMP_H, ACCEL_X_H
-    movff TEMP_L, ACCEL_X_L
+    movff TEMP_H, TARGET_X_H
+    movff TEMP_L, TARGET_X_L
 
     ; Same for Y
     movff ACCEL_Y_H, TEMP_H
     movff ACCEL_Y_L, TEMP_L
+    movff DELTA_Y_H, TEMP_DELTA_H
+    movff DELTA_Y_L, TEMP_DELTA_L
+    movff TOTAL_Y_H, TEMP_TOTAL_H
+    movff TOTAL_Y_L, TEMP_TOTAL_L
     call convert_tilt
-    movff TEMP_H, ACCEL_Y_H
-    movff TEMP_L, ACCEL_Y_L
+    movff TEMP_H, TARGET_Y_H
+    movff TEMP_L, TARGET_Y_L
 
     call move_motor_X
     call move_motor_Y
+    
 
     bra loop
     
-move_motor_X:
-    
-    movf ACCEL_X_L, W, A
-    cpfsgt TMR0L
-    return
-    
-    movf ACCEL_X_H, W
-    cpfsgt  TMR0H ; compare w and f, skip if greater than 
-    return
-    
-    bcf PORTD, 2, A
-    return
-
-move_motor_Y:
-    movf ACCEL_Y_L, W, A
-    movf ACCEL_Y_L, W
-    cpfsgt TMR0L
-    return
-    
-    movf ACCEL_Y_H, W
-    cpfsgt  TMR0H ; compare w and f, skip if greater than 
-    return
-    
-    bcf PORTD, 3, A
-    return
-
-; Divides value by 16 to be in range of servo
+; Divides value by 8 to be in range of servo
 convert_tilt:
-    movlw 0x03
-    call right_shift_W
     
-    movf DEFAULT_H, W, A
-    addwf TEMP_H, F
-
+    ; Shift actual tilt values
+    movff TEMP_H, SHIFT_H
+    movff TEMP_L, SHIFT_L
+    movlw 0x04
+    call right_shift_W
+    movff SHIFT_H, TEMP_H
+    movff SHIFT_L, TEMP_L
+    
+    ; Shift delta tilt values
+    movff TEMP_DELTA_H, SHIFT_H
+    movff TEMP_DELTA_L, SHIFT_L
+    movlw 0x04
+    call right_shift_W
+    movff SHIFT_H, TEMP_DELTA_H
+    movff SHIFT_L, TEMP_DELTA_L
+    
+    ; Shift integration values
+    movff TEMP_TOTAL_H, SHIFT_H
+    movff TEMP_TOTAL_L, SHIFT_L
+    movlw 0x00
+    call right_shift_W
+    movff SHIFT_H, TEMP_TOTAL_H
+    movff SHIFT_L, TEMP_TOTAL_L
+    
     movf DEFAULT_L, W, A
-    addwfc TEMP_L, w
+    addwf TEMP_L, F
+    subwf TEMP_DELTA_L, F
+   ;addwf TEMP_TOTAL_L, F
+
+    movf DEFAULT_H, W, A
+    addwfc TEMP_H, F
+    subwfb TEMP_DELTA_H, F
+    ;addwfc TEMP_TOTAL_H, F
     
     return
     
@@ -157,14 +250,14 @@ right_shift_W: ; Amount is stored in W
     movwf counter
 shift_loop:
     bcf	STATUS, 0
-    btfsc TEMP_H, 7
+    btfsc SHIFT_H, 7
     bsf	STATUS, 0
     
     ; Rotate high byte
-    rrcf TEMP_H, F
+    rrcf SHIFT_H, F
     
     ; Rotate low byte
-    rrcf TEMP_L, F
+    rrcf SHIFT_L, F
     
     ; Repeat 3 times for a shift of 3
     decfsz counter, F
